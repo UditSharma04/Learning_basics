61fb2c21240227279fd3eff9b2a2bf23e8dca262
Udit Sharma <109049436+UditSharma04@users.noreply.github.com>
2024-10-24 12:08:30 +0530

    developed the whole backend

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,15 @@ import productRoutes from "./routes/product.route.js"
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.use(express.json()); // middleware allows us to accept json data in req.body
 
-app.use("/api/proudcts", productRoutes); // /api/products will be added as prefix to all the routes in productRoutes
+app.use("/api/products", productRoutes); // /api/products will be added as prefix to all the routes in productRoutes
 
-app.listen(5000, ()=>{
+app.listen(PORT, ()=>{
     connectDB();
-    console.log("Server started at http://localhost:5000"); 
-});
+    console.log("Server started at http://localhost:" + PORT); 
+});  
 
+ 
