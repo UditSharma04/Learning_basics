5543a3e0582d12a3f98789604be380c1f6d305e1
Udit Sharma <109049436+UditSharma04@users.noreply.github.com>
2024-10-28 09:12:29 +0530

    update whole code so server can retrive from db

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,23 +1,25 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    price:{
-        type: Number,
-        required: true
-    },
-    image: {
-        type: String,
-        required:true
-    }
+const productSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		image: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true, // createdAt, updatedAt
+	}
+);
 
-}, {
-    timestamps: true // this is so that every entry will have a created at and updated at with it.
-});
-
-const Product = mongoose.model('Product',productSchema); // make a collection name Product with schema productSchema.
+const Product = mongoose.model("Product", productSchema);
 
 export default Product;
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createProduct, deleteProduct, getProducts, updatedProduct } from "../controllers/product.controller.js";
+
+import { createProduct, deleteProduct, getProducts, updateProduct } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.get("/", getProducts);
 router.post("/", createProduct);
-router.put("/:id", updatedProduct);
-router.delete("/:id",  deleteProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
